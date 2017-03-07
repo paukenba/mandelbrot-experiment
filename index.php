5f17f19922ffaef472861b4ba03b66c33f340683
Attila Vachter <paukenba@gmail.com>
2017-03-07 08:58:17 +0100

    object clone fixes

--- index.php
+++ index.php
@@ -3,19 +3,21 @@
 require_once 'Psr4AutoloaderClass.php';
 
 $autoloader = new Psr4AutoloaderClass();
-$autoloader->addNamespace('\Experiment', __DIR__.'/src/Experiment');
+$autoloader->addNamespace('Experiment', __DIR__.'/src/Experiment');
 $autoloader->register();
 
 $speed = 1.1;
 
-$coord = new \Experiment\Complex(-1.749721929742338571710438695186828716, 0.0000290166477536876274764422704374969315895);
+$coord = new Experiment\Complex(-1.749721929742338571710438695186828716, 0.0000290166477536876274764422704374969315895);
 
-$mandelbrot = new \Experiment\Mandelbrot(100, 40);
+$mandelbrot = new Experiment\Mandelbrot(100, 40);
 $rot = 0;
 for ($bound = 4; ; $bound /= $speed) {
     $iterations = min(20000, 100 + sqrt(1/$bound));
-    $from = (clone $coord)->add(new \Experiment\Complex(-$bound, $bound));
-    $to = (clone $coord)->add(new \Experiment\Complex($bound, -$bound));
+    $from = clone $coord;
+	$from->add(new Experiment\Complex(-$bound, $bound));
+    $to = clone $coord;
+	$to->add(new Experiment\Complex($bound, -$bound));
 
     echo "\033[1;1f"; // cursor to top-left corner
     echo $mandelbrot->setRotation($rot+=0.1)->render($from, $to, $iterations);
